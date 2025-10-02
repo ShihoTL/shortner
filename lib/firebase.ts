8845a2e1ab6c88bcabda5cce029b45dc1c4d7759
Anon <mgbekenjustice2@gmail.com>
2025-10-02 13:52:09 +0100

    chore(lurro): update 1 file(s)

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,7 +3,7 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  // These will need to be replaced with actual
+  // These will need to be replaced with
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "",
